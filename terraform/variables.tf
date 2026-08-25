79ad617ae286bf1db3e89a1cd55850a7d76e2a63
vaani <vaaniv41@gmail.com>
2026-08-25 12:05:53 +0530

    fix terraform configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,18 +10,17 @@ variable "project_name" {
   default     = "nestjs-products"
 }
 
-variable "app_image" {
-  description = "Initial image; CI/CD deploys later images."
-  type        = string
-  default     = "public.ecr.aws/docker/library/node:20-alpine"
-}
-
 variable "github_repository" {
   description = "GitHub repository in owner/repository format."
   type        = string
 }
 
-variable "container_port" {
+variable "app_port" {
   type    = number
   default = 5000
+}
+
+variable "ec2_instance_type" {
+  type    = string
+  default = "t3.micro"
 }
