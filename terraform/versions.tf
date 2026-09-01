0cfba5a60dcda84e48f532d65e0baad39051476c
vaani <vaaniv41@gmail.com>
2026-09-01 11:58:28 +0530

    modify

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -7,9 +7,13 @@ terraform {
       version = "~> 6.0"
     }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
+    tls= {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+     local = {
+      source = "hashicorp/local"
+      version = "~> 2.5"
     }
   }
 }
